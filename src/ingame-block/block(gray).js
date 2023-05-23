0adc05fc6490cf9f7b2acc827fa6f783aac30033
10cheon00 <10cheon00@gmail.com>
2023-05-23 20:18:04 +0900

    `block`, `jumpblock` 리팩토링
    Sprite를 상속받도록 변경

--- src/ingame-block/block(gray).js
+++ src/ingame-block/block(gray).js
@@ -1,28 +1,15 @@
-import { BoxCollider } from "../engine/data-structure/collider.js";
-import {
-  GameObject,
-  Circle,
-  Rect,
-  Vector,
-  Color,
-  InputManager,
-  ParticleEffect,
-  SceneManager,
-  Sprite,
-} from "/src/engine/module.js";
-import Path from "/src/engine/utils/path.js";
+import { Vector, Sprite } from "/src/engine/module.js";
 
-export default class Block extends GameObject {
+export default class Block extends Sprite {
   /**
-   * 기본블록입니다.
-   * 
+   * 기본 블록입니다.
+   *
    * @param {number} x - x좌표
    * @param {number} y - y좌표
    */
   constructor(x, y) {
-    super();
-    //setAssetFolderPath는 한 번만 실행해도 됩니다.
-    this.sprite = new Sprite({
+    super({
+      name: "block",
       imagePath: "/src/ingame-block/block(gray).png",
       transform: {
         position: new Vector(x, y),
@@ -32,6 +19,5 @@ export default class Block extends GameObject {
         isStatic: true,
       },
     });
-    this.addChild(this.sprite);
   }
 }
--- src/ingame-block/jumpblock.js
+++ src/ingame-block/jumpblock.js
@@ -1,27 +1,15 @@
-import { BoxCollider } from "../engine/data-structure/collider.js";
-import {
-  GameObject,
-  Circle,
-  Rect,
-  Vector,
-  Color,
-  InputManager,
-  ParticleEffect,
-  SceneManager,
-  Sprite,
-} from "/src/engine/module.js";
-import Path from "/src/engine/utils/path.js";
+import { Vector, Sprite } from "/src/engine/module.js";
 
-export default class JumpBlock extends GameObject {
+export default class JumpBlock extends Sprite {
   /**
    * 밟으면 더 높이 뛰어오르는 블록입니다.
-   * 
+   *
    * @param {number} x - x좌표
    * @param {number} y - y좌표
    */
   constructor(x, y) {
-    super();
-    this.sprite = new Sprite({
+    super({
+      name: "jumpblock",
       imagePath: "/src/ingame-block/jumpblock.png",
       transform: {
         position: new Vector(x, y),
@@ -31,11 +19,5 @@ export default class JumpBlock extends GameObject {
         isStatic: true,
       },
     });
-    
-    this.sprite.onCollision = (other) => {
-        this.circle.transform.velocity.y = -60;
-    };
-
-    this.addChild(this.sprite);
   }
 }
