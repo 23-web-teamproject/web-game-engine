4afe9b8ae302b5bee5d7c35edfea4ccaf9e00e5e
kimmn138 <kimmn138@naver.com>
2023-05-23 19:28:13 +0900

    Create jumpblock.js
    점프 블록

---
+++ src/ingame-block/jumpblock.js
@@ -0,0 +1,50 @@
+import { BoxCollider } from "../engine/data-structure/collider.js";
+import {
+  GameObject,
+  Circle,
+  Rect,
+  Vector,
+  Color,
+  InputManager,
+  ParticleEffect,
+  SceneManager,
+  Sprite,
+} from "/src/engine/module.js";
+import Path from "/src/engine/utils/path.js";
+
+export default class Stage1 extends GameObject {
+  constructor() {
+    super();
+    //setAssetFolderPath는 한 번만 실행해도 됩니다.
+    Path.setAssetFolderPath(import.meta.url); // 이 코드가 없으면 상대경로로 불러올 수 없습니다.
+    this.rect = new Rect({
+      width: 30,
+      height: 30,
+      isPhysicsEnable: true,
+      transform: {
+        position: new Vector(100,100),
+      },
+      rigidbody: {
+        bounceness: 1,
+        isGravity: true,
+      }
+    });
+    this.rect.onCollision = (other) => {
+        this.circle.transform.velocity.y = -60;
+    };
+    this.addChild(this.rect);
+
+    this.sprite = new Sprite({
+      imagePath: "jumpblock.png",
+      transform: {
+        position: new Vector(100, 100),
+      },
+      isPhysicsEnable: true,
+      rigidbody: {
+        isStatic: true,
+      },
+    });
+    this.addChild(this.sprite);
+
+  }
+}
