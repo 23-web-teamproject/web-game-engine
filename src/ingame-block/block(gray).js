65e1d9a8a59c53ec501b9a0610425d2c71630a36
Kim Taekcheon <10cheon00@gmail.com>
2023-05-27 01:38:47 +0900

    공과 블록간 상호작용 변경 (#10)
    * Update ball.js
    
    * 블록 구조 변경
    
    * 오른쪽 왼쪽 이동 블럭 수정
    
    * 점프블록과 블록 옆면 수정
    
    점프 블록도 위에서만 뛰게 수정
    블록들 옆면에 닿으면 중력이 켜지도록 수정
    
    ---------
    
    Co-authored-by: kimmn138 <kimmn138@naver.com>

--- src/ingame-block/block(gray).js
+++ src/ingame-block/block(gray).js
@@ -1,6 +1,6 @@
-import { Vector, Sprite } from "/src/engine/module.js";
+import { Vector, Sprite, GameObject, Rect } from "/src/engine/module.js";
 
-export default class Block extends Sprite {
+export default class Block extends GameObject {
   /**
    * 기본 블록입니다.
    *
@@ -8,16 +8,31 @@ export default class Block extends Sprite {
    * @param {number} y - y좌표
    */
   constructor(x, y) {
-    super({
-      name: "block",
-      imagePath: "/src/ingame-block/block(gray).png",
+    super();
+    this.addChild(new Rect({
+      name: "jumpTrigger",
+      width: 30,
+      height: 4,
       transform: {
-        position: new Vector(x, y),
+        position: new Vector(x, y - 13),
       },
       isPhysicsEnable: true,
       rigidbody: {
         isStatic: true,
       },
-    });
+    }));
+
+    // 블록 이미지
+    this.addChild(new Sprite({
+      name: "block(gray)",
+      imagePath: "/src/ingame-block/block(gray).png",
+      isPhysicsEnable: true,
+      transform: {
+        position: new Vector(x, y),
+      },
+      rigidbody: {
+        isStatic: true,
+      },
+    }))
   }
 }
--- src/ingame-block/jumpblock.js
+++ src/ingame-block/jumpblock.js
@@ -1,6 +1,6 @@
-import { Vector, Sprite } from "/src/engine/module.js";
+import { Vector, Sprite, Rect, GameObject } from "/src/engine/module.js";
 
-export default class JumpBlock extends Sprite {
+export default class JumpBlock extends GameObject {
   /**
    * 밟으면 더 높이 뛰어오르는 블록입니다.
    *
@@ -8,16 +8,32 @@ export default class JumpBlock extends Sprite {
    * @param {number} y - y좌표
    */
   constructor(x, y) {
-    super({
+    super();
+
+    this.addChild(new Rect({
       name: "jumpblock",
-      imagePath: "/src/ingame-block/jumpblock.png",
+      width: 26,
+      height: 1,
       transform: {
-        position: new Vector(x, y),
+        position: new Vector(x, y - 15),
       },
       isPhysicsEnable: true,
       rigidbody: {
         isStatic: true,
+        isTrigger: true,
       },
-    });
+    }));
+
+    this.addChild(new Sprite({
+      name: "jumpblockimg",
+      imagePath: "/src/ingame-block/jumpblock.png",
+      isPhysicsEnable: true,
+      transform: {
+        position: new Vector(x, y),
+      },
+      rigidbody: {
+        isStatic: true,
+      },
+    }));
   }
 }
