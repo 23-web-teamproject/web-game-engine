decc5956848e971f99fb322ce4ffc912836b77bb
rtaeho <ryou0920@naver.com>
2023-05-28 16:43:27 +0900

    ㅇ
    ㅇ

---
+++ src/ingame-block/wall.js
@@ -0,0 +1,26 @@
+import { Vector, Sprite, GameObject, Rect } from "/src/engine/module.js";
+
+export default class Block extends GameObject {
+  /**
+   * 기본 블록입니다.
+   *
+   * @param {number} x - x좌표
+   * @param {number} y - y좌표
+   */
+  constructor(x, y) {
+    super();
+
+    // 블록 이미지
+    this.addChild(new Sprite({
+      name: "wall",
+      imagePath: "/src/ingame-block/wall.png",
+      isPhysicsEnable: false,
+      transform: {
+        position: new Vector(x, y),
+      },
+      rigidbody: {
+        isStatic: true,
+      },
+    }))
+  }
+}
