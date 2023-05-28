a19190b87f573586165fb3020b9f6bbaad4c066c
10cheon00 <10cheon00@gmail.com>
2023-05-28 18:50:18 +0900

    좌우 이동 블록 구현
    잘 작동하는지 테스트해야합니다.

--- src/ingame-block/movingblock.js
+++ src/ingame-block/movingblock.js
@@ -1,19 +1,8 @@
-import { BoxCollider } from "../engine/data-structure/collider.js";
-import {
-  GameObject,
-  Circle,
-  Rect,
-  Vector,
-  Color,
-  InputManager,
-  ParticleEffect,
-  SceneManager,
-  Sprite,
-} from "/src/engine/module.js";
+import { GameObject, Rect, Vector, Sprite } from "/src/engine/module.js";
 import Path from "/src/engine/utils/path.js";
 
 export default class movingblcok extends GameObject {
-  constructor(x,y) {
+  constructor(x, y) {
     super();
     //setAssetFolderPath는 한 번만 실행해도 됩니다.
     Path.setAssetFolderPath(import.meta.url); // 이 코드가 없으면 상대경로로 불러올 수 없습니다.
@@ -27,15 +16,11 @@ export default class movingblcok extends GameObject {
       },
       isPhysicsEnable: true,
       rigidbody: {
-        isStatic: true,
+        isTrigger: true,
       },
     });
-  
-    this.rect.onCollision = (other) => {
-      this.rect.destroy();
-    }
     this.addChild(this.rect);
-    
+
     this.sprite = new Sprite({
       imagePath: "movingblock.png",
       transform: {
@@ -43,31 +28,58 @@ export default class movingblcok extends GameObject {
       },
       isPhysicsEnable: true,
       rigidbody: {
-        isStatic: false,
+        isStatic: true,
       },
-     });
-    this.direction = 1;
+    });
+    this.addChild(this.sprite);
 
+    this.isTriggered = false;
+
+    // 엔진 내에서 한 프레임에 Trigger가 여러번 호출되므로
+    // 당장은 최초 호출 시에만 처리할 수 있도록 함.
     this.sprite.onCollision = (other) => {
-      if (other.getName() === "wall") {
-        this.direction *= -1;
+      if (other.getName() === "wall" && this.isTriggered === false) {
+        this.isTriggered = true;
+
+        //트리거 블록과 이 블록간의 충돌된 영역을 구함.
+        const xSize = other.getSize().x + this.sprite.getSize().x;
+        const distance = Math.abs(
+          other.getPosition().x - this.sprite.getPosition().x
+        );
+        this.xDiff = xSize - distance;
       }
     };
-    
-    this.addChild(this.sprite);
+
+    this.direction = 1;
   }
+
   update(deltaTime) {
     super.update(deltaTime);
-    this.movingdirection(this.direction);
+    this.changeDirectionIfBlockTriggerWithWall();
+    this.moveByDirection(deltaTime);
   }
-  movingdirection(direction) {
-    if (direction === 1) {
-      this.rigidbody.isGravity = false;
-      this.addPosition(new Vector(2, 0));
-    }
-    if (direction === -1) {
-      this.rigidbody.isGravity = false;
-      this.addPosition(new Vector(-2, 0));
+
+  /**
+   * direction에 따라서 블록을 움직인다.
+   * 이 때 jumpTrigger도 따라서 움직인다.
+   * @param {number} deltaTime
+   */
+  moveByDirection(deltaTime) {
+    this.sprite.addPosition(new Vector(this.direction * 100 * deltaTime, 0));
+    this.rect.setPosition(this.sprite.getPosition().minus(new Vector(0, 13)));
+  }
+
+  /**
+   * 만약 트리거 블록과 충돌한 상태라면 direction을 전환한다.
+   * 이 때 다음 프레임에서 충돌된 상태를 피하기 위해
+   * 트리거 블록으로부터 이 블록을 떨어뜨려 놓는다.
+   */
+  changeDirectionIfBlockTriggerWithWall() {
+    if (this.isTriggered) {
+      this.direction *= -1;
+      this.sprite.matrix.x += this.xDiff * this.direction;
+      this.isTriggered = false;
+      console.log("asfd")
     }
   }
 }
--- src/ingame-block/wall.js
+++ src/ingame-block/wall.js
@@ -1,6 +1,6 @@
 import { Vector, Sprite, GameObject, Rect } from "/src/engine/module.js";
 
-export default class wall extends GameObject {
+export default class wall extends Sprite {
   /**
    * 기본 블록입니다.
    *
@@ -8,10 +8,7 @@ export default class wall extends GameObject {
    * @param {number} y - y좌표
    */
   constructor(x, y) {
-    super();
-
-    // 블록 이미지
-    this.addChild(new Sprite({
+    super({
       name: "wall",
       imagePath: "/src/ingame-block/wall.png",
       isPhysicsEnable: true,
@@ -19,8 +16,8 @@ export default class wall extends GameObject {
         position: new Vector(x, y),
       },
       rigidbody: {
-        isStatic: true,
+        isTrigger: true,
       },
-    }))
+    });
   }
 }
